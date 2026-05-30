3e3087893cbe17119066a59c9294fe11c5918c53
gsandss <gavinsands11@gmail.com>
2026-05-29 22:21:25 -0500

    progress

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 
 function Navbar(){
     return(
-        
         <nav className="w-full h-18 bg-gray-800 text-white flex items-center justify-center border-b-2 border-white">
-                <h1 className="text-lg font-bold">
-                    DSA Visualization Tool
-                </h1>
+            <h1 className="text-lg font-bold">
+                DSA Visualization Tool
+            </h1>
         </nav>
 
     )
