a05084bfd4616ed9678590a9619899d63c5672a3
gsandss <gavinsands11@gmail.com>
2026-05-29 22:21:15 -0500

    framework progress

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+            <p className="text-sm">&copy; 2026 Gavin Sands. All rights reserved.</p>
+        </div>
+    )   
+}
+
+export default Footer;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+function Navbar(){
+    return(
+        
+        <nav className="w-full h-18 bg-gray-800 text-white flex items-center justify-center border-b-2 border-white">
+                <h1 className="text-lg font-bold">
+                    DSA Visualization Tool
+                </h1>
+        </nav>
+
+    )
+}
+
+export default Navbar;
