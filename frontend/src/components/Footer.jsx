16f562dd33e209ceb05cc790bbaadb552ece8584
gsandss <gavinsands11@gmail.com>
2026-06-04 22:25:59 -0500

    footer change and file arrangement

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
-            <p className="text-sm">&copy; 2026 Gavin Sands. All rights reserved.</p>
+            <p className="text-sm">Created by <span className="font-bold">Gavin Sands</span></p>
         </div>
     )   
 }
